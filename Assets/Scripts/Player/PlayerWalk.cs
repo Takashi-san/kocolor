1c81584d73fd92b4b6107746664702e6d0ab465e
bruno takashi <bt.tengan@gmail.com>
2019-12-14 19:40:01 -0300

    Animação andar MC

--- Assets/Scripts/Player/PlayerWalk.cs
+++ Assets/Scripts/Player/PlayerWalk.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class PlayerWalk : MonoBehaviour {
 	Rigidbody2D _rb;
 	InputManager _inputManager;
+	[SerializeField] Animator _animator;
+	[SerializeField] SpriteRenderer _spriteR;
 	[SerializeField] float _velocity = 0;
 
 	void Start() {
@@ -22,5 +24,13 @@ public class PlayerWalk : MonoBehaviour {
 	void FixedUpdate() {
 		Vector2 movementVelocity = new Vector2(_inputManager.GetHorizontal() * _velocity, _rb.velocity.y);
 		_rb.velocity = movementVelocity;
+
+		_animator.SetFloat("speed", Mathf.Abs(movementVelocity.x));
+		if (Mathf.Sign(movementVelocity.x) == -1) {
+			_spriteR.flipX = true;
+		}
+		else if (movementVelocity.x > 0) {
+			_spriteR.flipX = false;
+		}
 	}
 }
