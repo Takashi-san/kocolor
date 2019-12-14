c6769f419172361a45f76f6b435b40f0b83e8746
bruno takashi <bt.tengan@gmail.com>
2019-12-14 00:40:07 -0300

    Mover um objeto 2D com rigidbody2D

---
+++ Assets/Scripts/Player/PlayerWalk.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerWalk : MonoBehaviour {
+	Rigidbody2D _rb;
+	InputManager _inputManager;
+	[SerializeField] float _velocity = 0;
+
+	void Start() {
+		_rb = gameObject.GetComponent<Rigidbody2D>();
+		if (!_rb) {
+			Debug.Log("No RigidBody2D here!");
+		}
+
+		_inputManager = FindObjectOfType<InputManager>().GetComponent<InputManager>();
+	}
+
+	void FixedUpdate() {
+		_rb.velocity = (Vector2.up * _inputManager.GetVertical() + Vector2.right * _inputManager.GetHorizontal()) * _velocity;
+	}
+}
