cc7e108e3c3f3e124a4ff2ed820b0812d204ded1
bruno takashi <bt.tengan@gmail.com>
2019-12-15 01:27:18 -0300

    Fog do ship

---
+++ Assets/Scripts/FogAutoScroll.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FogAutoScroll : MonoBehaviour {
+	Rigidbody2D _rb;
+	[SerializeField] float _velocity;
+	bool _once = false;
+
+	void Start() {
+		_rb = GetComponent<Rigidbody2D>();
+	}
+
+	void Update() {
+		if (!_once) {
+			if (Mathf.Abs(transform.position.y) < 1) {
+				Instantiate(gameObject, new Vector3(0, 11, 0), Quaternion.identity);
+				_once = true;
+			}
+		}
+
+		if (transform.position.y < -11) {
+			Destroy(gameObject);
+		}
+	}
+
+	void FixedUpdate() {
+		_rb.velocity = Vector2.down * _velocity;
+	}
+}
