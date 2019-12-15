a853fda3e5e225a8acc1d601c401a4a500548251
bruno takashi <bt.tengan@gmail.com>
2019-12-15 02:31:27 -0300

    Blobs e lógica de seguir ship.

---
+++ Assets/Scripts/Blobs.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Blobs : MonoBehaviour {
+	GameObject _player;
+	Rigidbody2D _rb;
+	[SerializeField] float _velocity = 0;
+	[SerializeField] int _order = 0;
+	[SerializeField] float _offset = 0;
+
+	void Start() {
+		_player = FindObjectOfType<ShipMovement>().gameObject;
+		_rb = GetComponent<Rigidbody2D>();
+	}
+
+	void FixedUpdate() {
+		Vector2 distance = _player.transform.position - transform.position;
+		if (distance.magnitude > _offset * _order) {
+			_rb.velocity = distance.normalized * _velocity;
+		}
+		else {
+			_rb.velocity *= 0.5f;
+		}
+	}
+}
--- Assets/Scripts/Ship/ShipHealth.cs
+++ Assets/Scripts/Ship/ShipHealth.cs
@@ -5,9 +5,28 @@ using UnityEngine;
 public class ShipHealth : MonoBehaviour {
 	[SerializeField] int _hp = 5;
 	[SerializeField] int _maxHp = 5;
+	[SerializeField] GameObject[] _blobs = new GameObject[5];
 
 	void Start() {
 		_hp = _maxHp;
+
+		switch (_maxHp) {
+			case 5:
+				Instantiate(_blobs[4], transform.position, Quaternion.identity);
+				goto case 4;
+			case 4:
+				Instantiate(_blobs[3], transform.position, Quaternion.identity);
+				goto case 3;
+			case 3:
+				Instantiate(_blobs[2], transform.position, Quaternion.identity);
+				goto case 2;
+			case 2:
+				Instantiate(_blobs[1], transform.position, Quaternion.identity);
+				goto case 1;
+			case 1:
+				Instantiate(_blobs[0], transform.position, Quaternion.identity);
+				break;
+		}
 	}
 
 	public void Damage(int dmg) {
