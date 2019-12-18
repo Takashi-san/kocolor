9ce2023e3e9eaf684451707b6e3a9b42044a614c
Gabriel Trindade <gabrielsvtrindade@gmail.com>
2019-12-17 22:39:14 -0300

    Pause afeta o audio

---
+++ Assets/Scripts/LevelChanger.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class LevelChanger : MonoBehaviour
+{
+
+    public Animator animator;
+
+    public void FadeToLevel(){
+        animator.SetTrigger("FadeOut");
+    }
+
+}
